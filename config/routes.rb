8ad9d01854d1b283523bc77c434e32a0c6f28e2c
Nakamura <gireru@hotmail.co.jp>
2019-03-18 15:08:23 +0900

    ログイン機能(devise)とhomeの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/home'
+  devise_for :users
   get 'homes/login'
+  root "homes#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
